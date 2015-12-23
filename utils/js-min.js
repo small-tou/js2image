b0cb3a160c3342c1034c1f6f79ce5243cad2f9f1
sunxinyu <sunxinyu@souche.com>
2015-12-23 14:10:53 +0800

    容错处理

--- utils/js-min.js
+++ utils/js-min.js
@@ -4,12 +4,17 @@ var fs = require("fs");
 var Promise = require("bluebird");
 module.exports = function(jsPath,callback){
     return new Promise(function(resolve,reject){
-        var source = fs.readFileSync(jsPath, "utf8");
-        var ast = UglifyJS.parse(source);
-        ast.figure_out_scope();
-        ast.compute_char_frequency();
-        ast.mangle_names();
-        var code = ast.print_to_string();
+        try{
+            var source = fs.readFileSync(jsPath, "utf8");
+            var ast = UglifyJS.parse(source);
+            ast.figure_out_scope();
+            ast.compute_char_frequency();
+            ast.mangle_names();
+            var code = ast.print_to_string();
+        }catch(e){
+            console.log('js minify error:',e)
+        }
+
         resolve(code);
     })
 }
